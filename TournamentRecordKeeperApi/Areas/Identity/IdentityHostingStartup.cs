79f42f27489ecfbe38edf7660074cfd9db9d943b
Kieran Tsai <kierantsai@gmail.com>
2020-09-23 19:11:55 +1200

    Added Identity UI source

---
+++ TournamentRecordKeeperApi/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TournamentRecordKeeperApi.Data;
+using TournamentRecordKeeperApi.Models;
+
+[assembly: HostingStartup(typeof(TournamentRecordKeeperApi.Areas.Identity.IdentityHostingStartup))]
+namespace TournamentRecordKeeperApi.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
