7c689d6bef39b512c5244209fa23a149ad83f6c8
Kieran Tsai <kierantsai@gmail.com>
2020-09-27 16:59:36 +1300

    Removed duplicate reference in appContext.cs

--- TournamentRecordKeeperApi/Data/appContext.cs
+++ TournamentRecordKeeperApi/Data/appContext.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
